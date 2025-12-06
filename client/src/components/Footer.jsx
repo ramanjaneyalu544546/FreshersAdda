15915dc8a5095b9bb5f6cd969b7d70d58d8a1ec9
ramanjaneyalu <ramanjaneyaluvenna@gmail.com>
2025-12-06 14:08:09 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sahand's
+                Freshers Adda
               </span>
               Blog
             </Link>
@@ -22,18 +22,18 @@ export default function FooterCom() {
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.100jsprojects.com'
+                  href='https://www.linkedin.com/in/ramanjaneyalu-reddy-venna-4907382b7'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  100 JS Projects
+                  Freshers Adda
                 </Footer.Link>
                 <Footer.Link
                   href='/about'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand's Blog
+                  Freshers Adda
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -41,11 +41,11 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://www.linkedin.com/in/ramanjaneyalu-reddy-venna-4907382b7'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Github
+                  Linkedin
                 </Footer.Link>
                 <Footer.Link href='#'>Discord</Footer.Link>
               </Footer.LinkGroup>
@@ -63,14 +63,14 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Sahand's blog"
+            by="Freshers Adda"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href='#' icon={BsFacebook}/>
             <Footer.Icon href='#' icon={BsInstagram}/>
             <Footer.Icon href='#' icon={BsTwitter}/>
-            <Footer.Icon href='https://github.com/sahandghavidel' icon={BsGithub}/>
+            <Footer.Icon href='https://www.linkedin.com/in/ramanjaneyalu-reddy-venna-4907382b7' icon={BsGithub}/>
             <Footer.Icon href='#' icon={BsDribbble}/>
 
           </div>
