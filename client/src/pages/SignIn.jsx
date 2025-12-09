f243a1b56b88347eb0986eaa02e0affda8b58674
ramanjaneyalu <ramanjaneyaluvenna@gmail.com>
2025-12-09 20:40:12 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -98,7 +98,7 @@ export default function SignIn() {
                 'Sign In'
               )}
             </Button>
-            //<OAuth />
+            /*<OAuth />*/
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Dont Have an account?</span>
