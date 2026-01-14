48ca58b30a2780cc23cd186d2d132875f246a872
ramanjaneyalu <ramanjaneyaluvenna@gmail.com>
2026-01-14 23:04:47 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:"AIzaSyDFgev_aDZuV_zL2iu6Dm1e7Ma_ceUJHls",
+  apiKey:"AIzaSyDFg7Ma_ceUJHls",
   authDomain:"blogfinal-61ed9.firebaseapp.com",
   projectId:"blogfinal-61ed9",
   storageBucket:"blogfinal-61ed9.firebasestorage.app",
